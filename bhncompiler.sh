011b57643b8713534d4c6a0eef7c803556b381fd
wiggle <40140554+WiggleDEV@users.noreply.github.com>
2018-07-07 19:51:49 +0200

    add dependencies and swap

--- bhncompiler.sh
+++ bhncompiler.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo -e "Installing dependencies, press enter if asked"
+delay 5
+apt-get update && apt-get upgrade && apt-get -y install software-properties-common && apt-get -y install build-essential  && apt-get -y install libtool autotools-dev autoconf automake  && apt-get -y install libssl-dev && apt-get -y install libevent-dev && apt-get -y install libboost-all-dev && apt-get -y install pkg-config  && add-apt-repository ppa:bitcoin/bitcoin && apt-get update && apt-get -y install libdb4.8-dev && apt-get -y install libdb4.8++-dev && apt-get -y install libminiupnpc-dev libzmq3-dev libevent-pthreads-2.0-5 && apt-get -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev && apt-get -y install libqrencode-dev bsdmainutils && apt install git && cd /var && touch swap.img && chmod 600 swap.img && dd if=/dev/zero of=/var/swap.img bs=1024k count=2000 && mkswap /var/swap.img && swapon /var/swap.img && echo '/var/swap.img none swap sw 0 0 ' >> /etc/fstab && cd ~
 echo -e "Setting permissions"
 chmod +x autogen.sh 
 chmod +x share/genbuild.sh
